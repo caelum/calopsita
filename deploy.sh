350e29300d1a9b7d0762b6af95279511febddb48
lucascs <lucascs@caelum-laptop.(none)>
2009-04-03 18:13:43 -0300

    changing deploy to stage on calopsita-lastBuild

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 killall -15 java
 
 cd hsqldb
-java -cp hsqldb.jar org.hsqldb.Server -database.0 file:calopsita -dbname.0 calopsita -port 9005 &
+nohup java -cp hsqldb.jar org.hsqldb.Server -database.0 file:calopsita -dbname.0 calopsita -port 9005 &
 
 sleep 3
-rm -rf ~/calopsita/*
-unzip -o ~/calopsita-staging.war -d ~/calopsita
-rm ~/calopsita/WEB-INF/lib/jstl*.jar ~/calopsita/WEB-INF/lib/standard*.jar
+rm -rf ~/calopsita-lastBuild/*
+unzip -o ~/calopsita-staging.war -d ~/calopsita-lastBuild
+rm ~/calopsita-lastBuild/WEB-INF/lib/jstl*.jar ~/calopsita-lastBuild/WEB-INF/lib/standard*.jar
 ~/jetty/bin/jetty.sh start
