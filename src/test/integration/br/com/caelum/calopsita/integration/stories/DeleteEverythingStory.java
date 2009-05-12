2f2f0f7f2645629c1f6eddf84783a643c50b5b4f
Caue Guerra <caue.guerra@gmail.com>
2009-05-12 16:38:30 -0300

    starting new story

---
+++ src/test/integration/br/com/caelum/calopsita/integration/stories/DeleteEverythingStory.java
@@ -0,0 +1,13 @@
+package br.com.caelum.calopsita.integration.stories;
+
+import br.com.caelum.calopsita.integration.stories.common.DefaultStory;
+
+/**
+ * <b>In order to</b> be able to make a mistake with no fear <br>
+ * <b>As</b> Fabs<br>
+ * <b>I want to</b> delete stories, iterations and projects that I have created<br>
+ * 
+ */
+public class DeleteEverythingStory extends DefaultStory {
+
+}
