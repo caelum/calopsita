096cfc7baf9ed6d8f2de7fa6e0b3ddccf666f471
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-05-14 11:55:50 -0300

    implementing deletion

--- src/main/java/br/com/caelum/calopsita/logic/ProjectLogic.java
+++ src/main/java/br/com/caelum/calopsita/logic/ProjectLogic.java
@@ -47,8 +47,13 @@ public class ProjectLogic {
     	this.stories = this.repository.listStoriesFrom(project);
     }
 
-    public void delete(Project project) {
-    	
+    public String delete(Project project) {
+    	Project loaded = this.repository.load(project);
+    	if (!currentUser.equals(loaded.getOwner())) {
+    		return "invalid";
+    	}
+    	this.repository.remove(loaded);
+    	return "ok";
     }
 	public List<User> getUsers() {
 		return users;
