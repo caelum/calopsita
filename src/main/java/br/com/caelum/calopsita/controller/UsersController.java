36db9bdce741dcc1e34d559594da0b02d15aa7c5
Caue Guerra <caue.guerra@gmail.com>
2009-05-14 23:12:03 -0300

    user controller migrated

--- src/main/java/br/com/caelum/calopsita/controller/UsersController.java
+++ src/main/java/br/com/caelum/calopsita/controller/UsersController.java
@@ -1,56 +1,68 @@
 package br.com.caelum.calopsita.controller;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
 import javax.servlet.http.HttpSession;
 
 import org.vraptor.annotations.InterceptedBy;
-import org.vraptor.i18n.Message;
-import org.vraptor.validator.ValidationErrors;
 
 import br.com.caelum.calopsita.infra.interceptor.HibernateInterceptor;
 import br.com.caelum.calopsita.model.User;
 import br.com.caelum.calopsita.repository.UserRepository;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Validator;
+import br.com.caelum.vraptor.validator.Hibernate;
+import br.com.caelum.vraptor.validator.Validations;
 
 @Resource
 @InterceptedBy( { HibernateInterceptor.class })
 public class UsersController {
     private final UserRepository repository;
     private final HttpSession session;
+    private final Validator validator;
 
-    public UsersController(UserRepository repository, HttpSession session) {
+    public UsersController(Validator validator, UserRepository repository, HttpSession session) {
+        this.validator = validator;
         this.repository = repository;
         this.session = session;
     }
 
-    public void formSignUp() {
-
+    @Path("/users/new") @Get
+    public User form() {
+        return new User();
     }
 
-    public void save(User user) {
+    @Path("/users") @Post
+    public void save(final User user) {
+        validator.checking(new Validations() {
+            {
+                that(repository.find(user.getLogin())).shouldBe(null);
+                and(Hibernate.validate(user));
+            }
+        });
         this.repository.add(user);
         this.session.setAttribute(User.class.getName(), user);
         this.session.setAttribute("currentUser", user);
     }
 
-    public void validateSave(ValidationErrors errors, User user) {
-        User found = repository.find(user.getLogin());
-        if (found != null) {
-            errors.add(new Message("user.save", "user.already.exists"));
-        }
-    }
-
-    public void login(User user) {
+    @Path("/users/login") @Get
+    public void login(final User user) {
+        validator.checking(new Validations() {
+            {
+                User found = repository.find(user.getLogin());
+                that(found).shouldBe(notNullValue());
+                that(found.getPassword()).shouldBe(equalTo(user.getPassword()));
+            }
+        });
         this.session.setAttribute(User.class.getName(), user);
         this.session.setAttribute("currentUser", user);
     }
 
-    public void validateLogin(ValidationErrors errors, User user) {
-        User found = repository.find(user.getLogin());
-        if (found == null || !found.getPassword().equals(user.getPassword())) {
-            errors.add(new Message("user.login", "login.invalid"));
-        }
-    }
-
+    @Path("/users/logout") @Get
     public void logout() {
         this.session.removeAttribute(User.class.getName());
         this.session.removeAttribute("currentUser");
