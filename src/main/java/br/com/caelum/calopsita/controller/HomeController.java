c741500e90ef638ab664c5cd5088e4343109f47f
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-05-15 18:18:26 -0300

    adding slashes to the end of paths

--- src/main/java/br/com/caelum/calopsita/controller/HomeController.java
+++ src/main/java/br/com/caelum/calopsita/controller/HomeController.java
@@ -3,16 +3,12 @@ package br.com.caelum.calopsita.controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
-import br.com.caelum.vraptor.Result;
+
 @Resource
 public class HomeController {
-	private final Result result;
-
-	public HomeController(Result result) {
-		this.result = result;
-	}
-    @Get //@Path("/home/login/")
-    public void login() {
+    @Get @Path("/home/login/")
+    public User login() {
+        return new User();
     }
 
     @Path("/users/notAllowed/") @Get
