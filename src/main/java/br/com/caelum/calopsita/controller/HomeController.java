7980f6dbe378cc3acba6b1107e7c39c2cbce892a
Caue Guerra <caue.guerra@gmail.com>
2009-05-14 23:16:54 -0300

    home controller migrated

--- src/main/java/br/com/caelum/calopsita/controller/HomeController.java
+++ src/main/java/br/com/caelum/calopsita/controller/HomeController.java
@@ -1,12 +1,18 @@
 package br.com.caelum.calopsita.controller;
 
+import br.com.caelum.calopsita.model.User;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 
 @Resource
 public class HomeController {
-    public void login() {
+    @Path("/home/login") @Get
+    public User login() {
+        return new User();
     }
     
+    @Path("/users/notAllowed") @Get
     public void notAllowed() {
     }
 }
