60e574e3b17c275a94e166a1d8bc79f3aff2cf49
Caue Guerra <caue.guerra@gmail.com>
2009-05-25 17:27:14 -0300

    fixing tests

--- src/main/java/br/com/caelum/calopsita/repository/ProjectRepository.java
+++ src/main/java/br/com/caelum/calopsita/repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package br.com.caelum.calopsita.repository;
 
 import java.util.List;
 
+import br.com.caelum.calopsita.model.Iteration;
 import br.com.caelum.calopsita.model.Project;
 import br.com.caelum.calopsita.model.Card;
 import br.com.caelum.calopsita.model.User;
