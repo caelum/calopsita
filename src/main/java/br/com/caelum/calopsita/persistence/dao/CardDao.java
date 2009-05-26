593a879fe669f195fe58a81c7350247329c4f972
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-05-26 17:32:40 -0300

    rename StoryDao to CardDao

--- src/main/java/br/com/caelum/calopsita/persistence/dao/CardDao.java
+++ src/main/java/br/com/caelum/calopsita/persistence/dao/CardDao.java
@@ -10,16 +10,17 @@ import br.com.caelum.calopsita.model.Gadget;
 import br.com.caelum.calopsita.model.Gadgets;
 import br.com.caelum.calopsita.model.Iteration;
 import br.com.caelum.calopsita.model.Project;
+import br.com.caelum.calopsita.model.Card;
 import br.com.caelum.calopsita.repository.CardRepository;
 
 public class CardDao implements CardRepository {
 
 	private final Session session;
-
+	
 	public CardDao(Session session) {
 		this.session = session;
 	}
-
+	
 	@Override
 	public void add(Card card) {
 		session.save(card);
@@ -37,7 +38,7 @@ public class CardDao implements CardRepository {
 	public void remove(Card card) {
 		session.delete(card);
 	}
-
+	
 	@Override
 	public List<Card> listFrom(Project project) {
 		return this.session.createQuery("select c from PrioritizableCard p right join p.card c " +
@@ -52,7 +53,7 @@ public class CardDao implements CardRepository {
 				"order by p.priority")
 				.setParameter("project", project).list();
 	}
-
+	
 	@Override
 	public List<Card> listSubcards(Card card) {
 		return session.createQuery("from Card s where s.parent = :card")
