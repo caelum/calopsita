812d5b478b51b3f52f25c558f28ae6dafde202e6
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-05-27 17:37:14 -0300

    relationships cant be primary keys =(

--- src/main/java/br/com/caelum/calopsita/model/PrioritizableCard.java
+++ src/main/java/br/com/caelum/calopsita/model/PrioritizableCard.java
@@ -1,15 +1,20 @@
 package br.com.caelum.calopsita.model;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+@Entity
 public class PrioritizableCard implements Gadget {
 
 	@Id
+	private Long id;
+
 	@OneToOne
 	private Card card;
 
 	private int priority;
+
 
 	public Card getCard() {
 		return card;
@@ -25,6 +30,14 @@ public class PrioritizableCard implements Gadget {
 
 	public void setPriority(int priority) {
 		this.priority = priority;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
 	}
 
 
