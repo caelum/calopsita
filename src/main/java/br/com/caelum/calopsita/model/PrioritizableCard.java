672f0e0606df8419e1b2ae052f6c82dabbfc458d
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-05-27 16:58:41 -0300

    adding attributes to PriorizableCard

--- src/main/java/br/com/caelum/calopsita/model/PrioritizableCard.java
+++ src/main/java/br/com/caelum/calopsita/model/PrioritizableCard.java
@@ -1,5 +1,31 @@
 package br.com.caelum.calopsita.model;
 
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 public class PrioritizableCard implements Gadget {
+
+	@Id
+	@OneToOne
+	private Card card;
+
+	private int priority;
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
 
 }
