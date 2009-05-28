6bb2c7323b1e061ca752ff94596b9dcac2a2f544
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-05-28 17:01:07 -0300

    creating PriorizationRepository

---
+++ src/main/java/br/com/caelum/calopsita/persistence/dao/PrioritizationDao.java
@@ -0,0 +1,56 @@
+package br.com.caelum.calopsita.persistence.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.transform.ResultTransformer;
+
+import br.com.caelum.calopsita.model.Card;
+import br.com.caelum.calopsita.model.PrioritizableCard;
+import br.com.caelum.calopsita.model.Project;
+import br.com.caelum.calopsita.repository.PrioritizationRepository;
+
+public class PrioritizationDao implements PrioritizationRepository {
+
+	private final Session session;
+
+	public PrioritizationDao(Session session) {
+		this.session = session;
+	}
+
+	@Override
+	public PrioritizableCard load(PrioritizableCard card) {
+		return (PrioritizableCard) session.load(PrioritizableCard.class, card.getId());
+	}
+
+	public List<List<Card>> listCards(Project project) {
+		return session.createQuery("from PrioritizableCard c where c.project = :project order by c.priority")
+			.setParameter("project", project)
+			.setResultTransformer(new ListResultTransformer())
+			.list();
+	}
+
+	private static class ListResultTransformer implements ResultTransformer {
+
+		@Override
+		public List transformList(List list) {
+			List<List<Card>> result = new ArrayList<List<Card>>();
+			List<PrioritizableCard> cards = list;
+			for (int i = 0; i < cards.get(list.size() - 1).getPriority(); i++) {
+				result.add(new ArrayList<Card>());
+			}
+			for (PrioritizableCard card : cards) {
+				result.get(card.getPriority()).add(card.getCard());
+			}
+			return result;
+		}
+
+		@Override
+		public Object transformTuple(Object[] objs, String[] names) {
+			return objs[0];
+		}
+
+	}
+
+}
