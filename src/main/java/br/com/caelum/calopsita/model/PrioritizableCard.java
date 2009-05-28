eb1bd458d1848edf3a1f431f0079e3ad37287320
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-05-28 15:34:18 -0300

    fixing mapping of prioritizableCard

--- src/main/java/br/com/caelum/calopsita/model/PrioritizableCard.java
+++ src/main/java/br/com/caelum/calopsita/model/PrioritizableCard.java
@@ -1,16 +1,25 @@
 package br.com.caelum.calopsita.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 @Entity
 public class PrioritizableCard implements Gadget {
 
 	@Id
+	@GeneratedValue(generator="custom")
+	@GenericGenerator(name="custom", strategy="foreign",
+			parameters=@Parameter(name="property", value="card"))
 	private Long id;
 
 	@OneToOne
+	@PrimaryKeyJoinColumn
 	private Card card;
 
 	private int priority;
