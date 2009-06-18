529f8bedcfcd016712aaea89e1fb413c9487735f
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-06-18 15:26:02 -0300

    adding planning card model

---
+++ src/main/java/br/com/caelum/calopsita/model/PlanningCard.java
@@ -0,0 +1,57 @@
+package br.com.caelum.calopsita.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+public class PlanningCard implements Gadget {
+
+	@Id
+	@GeneratedValue(generator = "custom")
+	@GenericGenerator(name = "custom", strategy = "foreign", parameters = @Parameter(name = "property", value = "card"))
+	private Long id;
+
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	private Card card;
+
+	private Iteration iteration;
+
+	public PlanningCard() {
+	}
+
+	public PlanningCard(Card card) {
+		this.card = card;
+	}
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setIteration(Iteration iteration) {
+		this.iteration = iteration;
+	}
+
+	public Iteration getIteration() {
+		return iteration;
+	}
+
+}
