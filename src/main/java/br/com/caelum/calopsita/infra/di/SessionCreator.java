64e42cd2739c84ed3c4ac177590389cfb21a9c9e
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-07-10 16:09:16 -0300

    new vraptor dependencies, and session managing

--- src/main/java/br/com/caelum/calopsita/infra/di/SessionCreator.java
+++ src/main/java/br/com/caelum/calopsita/infra/di/SessionCreator.java
@@ -1,5 +1,8 @@
 package br.com.caelum.calopsita.infra.di;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -14,14 +17,23 @@ public class SessionCreator implements ComponentFactory<Session> {
 	 */
 	private static final long serialVersionUID = -4411865493965531937L;
 	private final SessionFactory factory;
+	private org.hibernate.classic.Session session;
 
 	public SessionCreator(SessionFactory factory) {
 		this.factory = factory;
 	}
 
+	@PostConstruct
+	public void create() {
+		this.session = factory.openSession();
+	}
 	@Override
 	public Session getInstance() {
-		return factory.getCurrentSession();
+		return session;
+	}
+	@PreDestroy
+	public void destroy() {
+		this.session.close();
 	}
 
 
