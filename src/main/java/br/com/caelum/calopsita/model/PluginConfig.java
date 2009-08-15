242868ab0a9ea3e55ff5f28eb6b611ef00276525
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2009-08-14 22:34:46 -0300

    changing pluging interface a little

--- src/main/java/br/com/caelum/calopsita/model/PluginConfig.java
+++ src/main/java/br/com/caelum/calopsita/model/PluginConfig.java
@@ -1,11 +1,10 @@
 package br.com.caelum.calopsita.model;
 
-import br.com.caelum.vraptor.core.MethodInfo;
 
 public interface PluginConfig {
 
 	String getName();
 
-	void activate(Menu menu, MethodInfo parameters);
+	void includeMenus(Menu menu);
 
 }
